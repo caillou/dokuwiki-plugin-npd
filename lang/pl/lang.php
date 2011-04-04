1288652826426c6ccbfe161ad1238d7dd6528a69
Matthias Schulte <post@lupo49.de>
2011-04-04 20:02:44 +0200

    added localizations: Czech (cs), Japanse (jp), Polish (pl), Slovenian (sl)

---
+++ lang/pl/lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Polish language file for the "New Page Dialog" plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Łukasz Czerski
+ */
+ 
+/* buttons */
+$lang['btn_create_new_page'] = 'Utwórz nową stronę';
+$lang['btn_create_page'] = 'Utwórz stronę';
+$lang['btn_new_folder']  = 'Nowy katalog';
+$lang['btn_ok'] = 'OK';
+ 
+/* dialogs */
+$lang['dlg_confirm_page_name'] = 'Czy na pewno utworzyć stronę:\n';
+$lang['dlg_new_folder'] = 'Podaj nazwę katalogu:';
+ 
+/* misc */
+$lang['msc_page_title'] = 'Tytuł strony';
