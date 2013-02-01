7cffcbf3c2377bc28e77ac83a28b7708c0503543
René Corinth <corinth@cosmocode.de>
2013-02-01 11:48:22 +0100

    Remove untranslated languages

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -6,17 +6,25 @@
  * @author     Pierre Spring <pierre.spring[curly thing]liip[full stopW]ch>
  */
 
-/* buttons */
+// buttons
 $lang['btn_create_new_page'] = 'Create New Page';
 $lang['btn_create_page'] = 'Create Page';
 $lang['btn_new_folder']  = 'New Folder';
 $lang['btn_ok'] = 'OK';
 
-/* dialogs */
-$lang['dlg_confirm_page_name'] = 'Are you sure you want to \ncreate a page called:\n';
-$lang['dlg_new_folder'] = 'Enter new folder name:';
-
-/* misc */
+// misc
 $lang['msc_page_title'] = 'Page Title';
+$lang['js']['msc_page_title'] = 'Page Title';
 
-//Setup VIM: ex: et ts=4 :
+// Buttons
+$lang['js']['btn_create_page'] = 'Create Page';
+$lang['js']['btn_edit_page'] = 'Edit Page';
+
+// Dialogs
+$lang['js']['dlg_confirm_page_name'] = 'Are you sure you want to create a page called: ';
+$lang['js']['dialog_folder_name'] = 'Please enter the folder Name.<br> Note that a folder will <b>NOT</b> be created before a page<br> within the folder is <b>SAVED</b>.';
+$lang['js']['dlg_new_folder'] = 'Enter new folder name:';
+$lang['dlg_path_info'] = 'Selected Path:';
+
+// do not change this value
+$lang['js']['wiki_title'] = addcslashes($conf['title'], "'");
